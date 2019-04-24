8ba50b2a4c60f26486da3c12812912df5a4b9bd1
Rakesh Mistry <rakesh@qualica.com>
2019-04-24 22:16:36 +0200

    Style user input with validation

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="hello">
       <div class="holder">
+          <form @submit.prevent="addSkill">
+          <input type="text" placeholder="Input a skill that you have" v-model="skill" v-validate="'min:5'" name="skill">
+          <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+          </form>
           <ul>
               <li v-for="(data, index) in skills" :key='index'> {{ index }}. {{ data.skill }} </li>
           </ul>
 
-          <p v-if="skills.length >= 1"> You have more than 1 skills </p>
-          <p v-else>You have less than or equal to 1 skill</p>
+          <p>These are the skills that you possess.</p>
       </div>
   </div>
 </template>
@@ -16,30 +19,74 @@ export default {
     name: 'Skills',
     data() {
         return {
+            skill: '',
             skills: [
                 {"skill": "Vue.js"},
                 {"skill": "Frontend developer"}
-            ]
-
+            ],
         } 
+    },
+    methods: {
+        addSkill() {
+            this.$validator.validateAll().then((result) => {
+                if(result){
+                    this.skills.push({skill: this.skill});
+                    this.skill='';
+                    } else {
+                    console.log('not valid');
+                }
+
+            })
+        }
     }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-    margin: 40px 0 0;
+<style  scoped>
+.holder {
+    background: #fff;
 }
+
 ul {
-    list-style-type: none;
+    margin: 0;
     padding: 0;
+    list-style-type: none;
 }
-li {
+
+ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+}
+
+p {
+    text-align:center;
+    padding: 30px 0;
+    color: gray;
+}
+
+.container {
+    box-shadow: 0px 0px 40px lightgray;
+}
+
+input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+}
+
+.alert {
+    background: #fdf2ce;
+    font-weight: bold;
     display: inline-block;
-    margin: 0 10px;
-}
-a {
-    color: #42b983;
+    padding: 5px;
+    margin-top: -20px;
 }
 </style>
