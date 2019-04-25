61ccd5e714fe96ed6843dc79932f44566dbf403f
Rakesh Mistry <rakesh@qualica.com>
2019-04-25 19:11:58 +0200

    About page added. Remove item with font-awesome added.

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -9,7 +9,10 @@
           </form>
           <ul>
               <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
-              <li v-for="(data, index) in skills" :key='index'> {{ index }}. {{ data.skill }} </li>
+                  <li v-for="(data, index) in skills" :key='index'> 
+                      {{ index }}. {{ data.skill }}
+                      <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+              </li>
               </transition-group>
           </ul>
 
@@ -41,6 +44,9 @@ export default {
                 }
 
             })
+        },
+        remove(id) {
+            this.skills.splice(id,1);
         }
     }
 }
@@ -49,6 +55,8 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style  scoped>
 @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+
 .holder {
     background: #fff;
 }
@@ -66,6 +74,8 @@ ul li {
     border-left: 5px solid #3EB3F6;
     margin-bottom: 2px;
     color: #3E5252;
+    text-align: justify;
+  text-justify: inter-word;
 }
 
 p {
@@ -111,5 +121,9 @@ input {
   100% {
     transform: scale(1);
   }
+}
+i {
+  float: right;
+  color: red;
 }
 </style>
